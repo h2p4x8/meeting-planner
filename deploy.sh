4bfed8368d9812db144a21408633bab691f968c9
Elizaveta Voziyanova <44549195+h2p4x8@users.noreply.github.com>
2019-12-09 13:13:08 +0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+# остановить публикацию при ошибках
+set -e
+
+# сборка
+npm run build
+
+# переход в каталог сборки
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
