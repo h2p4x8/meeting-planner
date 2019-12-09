b425cd8f49ecdd933bd1d20958ec6eaf3d048eb0
Elizaveta Voziyanova <44549195+h2p4x8@users.noreply.github.com>
2019-12-09 13:12:23 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,9 @@ module.exports = {
     'vuetify',
   ],
 
-  lintOnSave: false
+  lintOnSave: false,
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/meeting-planner/'
+    : '/'
+}
 };
